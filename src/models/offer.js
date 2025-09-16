9958db84124052907d15d74ea8b66dfa2f415ac7
Vipin maurya <mauryavipin751@gmail.com>
2025-09-16 13:12:15 +0530

    Mongoose schema for product/offer details.

---
+++ src/models/offer.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const OfferSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value_props: { type: [String], default: [] },
+  ideal_use_cases: { type: [String], default: [] },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.Offer || mongoose.model("Offer", OfferSchema);
